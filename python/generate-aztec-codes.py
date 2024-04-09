7e1eb3f55a20ea72172215bbaa2b0383bcaf8602
Joseph Kramer <joseph.ryan.kramer@gmail.com>
2024-04-09 23:02:14 +0000

    generate aztec codes for alignments

--- python/generate-aztec-codes.py
+++ python/generate-aztec-codes.py
@@ -4,6 +4,18 @@ import treepoem
 
 image = treepoem.generate_barcode(
     barcode_type="azteccode",
-    data="LARPE",
+    data="DARK1",
 )
-image.convert("1").save("larpe.png")
+image.convert("1").save("DARK1.png")
+
+image = treepoem.generate_barcode(
+    barcode_type="azteccode",
+    data="LIGHT",
+)
+image.convert("1").save("LIGHT.png")
+
+image = treepoem.generate_barcode(
+    barcode_type="azteccode",
+    data="NEUTR",
+)
+image.convert("1").save("NEUTR.png")
