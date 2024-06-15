33c99fe01e1896808a5252fdd51d62358f262aaf
Joseph Kramer <joseph.ryan.kramer@gmail.com>
2024-06-15 18:24:14 +0000

    wip enable scanning

--- react-rebuild/src/App.tsx
+++ react-rebuild/src/App.tsx
@@ -4,18 +4,83 @@ import viteLogo from "/vite.svg";
 import "./App.css";
 import Logo from "./Logo";
 import Crate from "./Crate";
-import { CrateDecoder } from "./crate-decoder";
+import { CrateDecoder, CrateType } from "./crate-decoder";
+import Html5QrcodePlugin from "./Html5QrcodePlugin";
+import { Html5QrcodeResult } from "html5-qrcode";
+import { CrewManifest } from "./crew-manifest";
+import { ChainCodeDecoder } from "./chain-code";
+import { BadgeDecoder } from "./badge-decoder";
 
 function App() {
+  //read parameters from the url
+  const queryString = window.location.search;
+  console.log(queryString);
+  const urlParams = new URLSearchParams(queryString);
+
   const [count, setCount] = useState(0);
 
   const crateDecoder = new CrateDecoder();
-  const crateToDisplay = crateDecoder.decode("FAL16");
+  const crewMembers = new CrewManifest();
+  const chainCodeDecoder = new ChainCodeDecoder();
+  const badgeDecoder = new BadgeDecoder();
+
+  const [crateToDisplay, setCrateToDisplay] = useState(
+    crateDecoder.decode("FAL16"),
+  );
+
+  const onNewScanResult = (
+    decodedText: string,
+    decodedResult: Html5QrcodeResult,
+  ) => {
+    console.log(`Scan result ${decodedText}`, decodedResult);
+    //html5QrcodeScanner.clear();
+
+    if (badgeDecoder.isValidBadgeCode(decodedText)) {
+      badgeDecoder.add(decodedText);
+    } else {
+      const crate = crateDecoder.decode(decodedText);
+
+      if (
+        chainCodeDecoder.isValidChainCode(decodedText) ||
+        crate.type == CrateType.Unknown ||
+        urlParams.has("debug")
+      ) {
+        if (chainCodeDecoder.isValidChainCode(decodedText)) {
+          chainCodeDecoder.setChainCodeResult(decodedText, badgeDecoder);
+        } else {
+          crateDecoder.setResult(decodedText, badgeDecoder);
+        }
+      } else {
+        /*
+          //start puzzle and wait for success
+          puzzle.style.display = "block";
+          waitToSolvePuzzle().then(
+            function () {
+              console.log("PUZZLE SUCCESS");
+              crateDecoder.setResult(decodedText, badgeDecoder);
+              puzzle.style.display = "none";
+            },
+            function () {
+              console.log("PUZZLE FAILURE");
+            },
+          );
+          */
+        //crateDecoder.setResult(decodedText, badgeDecoder);
+        setCrateToDisplay(crateDecoder.decode(decodedText));
+      }
+    }
+  };
 
   return (
     <>
       <Logo />
       <Crate crate={crateToDisplay} />
+      <Html5QrcodePlugin
+        fps={10}
+        qrbox={250}
+        disableFlip={false}
+        qrCodeSuccessCallback={onNewScanResult}
+      />
       <div>
         <a href="https://vitejs.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
---
+++ react-rebuild/src/Html5QrcodePlugin.tsx
@@ -0,0 +1,70 @@
+import { Html5QrcodeScanner } from "html5-qrcode";
+import { Html5QrcodeScannerConfig } from "html5-qrcode/esm/html5-qrcode-scanner";
+import { useEffect } from "react";
+
+const qrcodeRegionId = "html5qr-code-full-region";
+
+// Creates the configuration object for Html5QrcodeScanner.
+const createConfig = (props: {
+  fps?: number;
+  qrbox?: number;
+  aspectRatio?: number;
+  disableFlip?: boolean;
+}) => {
+  const config: Html5QrcodeScannerConfig = {
+    fps: undefined,
+  };
+  if (props.fps) {
+    config.fps = props.fps;
+  }
+  if (props.qrbox) {
+    config.qrbox = props.qrbox;
+  }
+  if (props.aspectRatio) {
+    config.aspectRatio = props.aspectRatio;
+  }
+  if (props.disableFlip !== undefined) {
+    config.disableFlip = props.disableFlip;
+  }
+  return config;
+};
+
+const Html5QrcodePlugin = (props: {
+  verbose?: any;
+  qrCodeSuccessCallback?: any;
+  qrCodeErrorCallback?: any;
+  fps?: number;
+  qrbox?: number;
+  aspectRatio?: number;
+  disableFlip?: boolean;
+}) => {
+  useEffect(() => {
+    // when component mounts
+    const config = createConfig(props);
+    const verbose = props.verbose === true;
+    // Suceess callback is required.
+    if (!props.qrCodeSuccessCallback) {
+      throw "qrCodeSuccessCallback is required callback.";
+    }
+    const html5QrcodeScanner = new Html5QrcodeScanner(
+      qrcodeRegionId,
+      config,
+      verbose,
+    );
+    html5QrcodeScanner.render(
+      props.qrCodeSuccessCallback,
+      props.qrCodeErrorCallback,
+    );
+
+    // cleanup function when component will unmount
+    return () => {
+      html5QrcodeScanner.clear().catch((error) => {
+        console.error("Failed to clear html5QrcodeScanner. ", error);
+      });
+    };
+  }, []);
+
+  return <div id={qrcodeRegionId} />;
+};
+
+export default Html5QrcodePlugin;
