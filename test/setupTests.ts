edf6b0a18266f625a993020c35618db118717b3a
Joseph Kramer <joseph.ryan.kramer@gmail.com>
2024-07-21 19:35:21 +0000

    remove commented out code in setupTests.ts

--- test/setupTests.ts
+++ test/setupTests.ts
@@ -1,6 +1,3 @@
-//import mockLocalStorage from "./mockLocalStorage";
-//global.localStorage = mockLocalStorage;
-
 import { LocalStorage } from "node-localstorage";
 
 global.localStorage = new LocalStorage("./scratch");
