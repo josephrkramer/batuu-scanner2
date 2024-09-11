867347b6946b80a5d52bf42ada368e866b7c8825
Joseph Kramer <joseph.ryan.kramer@gmail.com>
2024-09-10 20:32:09 -0400

    359 rework buttons with icons (#365)
    * fix alignment question
    
    * rework multiple choice crate display

--- src/components/MultipeChoiceCrate.tsx
+++ src/components/MultipeChoiceCrate.tsx
@@ -1,4 +1,12 @@
-import { Button, Card, Flex, Image } from "antd";
+import {
+  Button,
+  Card,
+  Collapse,
+  CollapseProps,
+  Flex,
+  Image,
+  Typography,
+} from "antd";
 import { CrateContents, CrateDecoder } from "../services/crate-decoder";
 import Title from "antd/es/typography/Title";
 import { BadgeDecoder } from "../services/badge-decoder";
@@ -53,28 +61,34 @@ function childButtons(
     React.SetStateAction<string | undefined>
   >,
 ) {
-  const buttons = [];
-  for (const child of parent.multipleChoice) {
-    const button = (
-      <Button
-        size={"large"}
-        icon={
-          <Image src={child.image} preview={false} width={35} height={"auto"} />
-        }
-        key={child.contents}
-        onClick={() => {
-          crateDecoder.addToScannedMultipleChoice(child.code, child);
-          crateDecoder.setResult(child.code, badgeDecoder);
-          setCrateToDisplay(child);
-          setRenderMultipleChoiceCrateCode(undefined);
-        }}
-      >
-        {child.contents}
-      </Button>
-    );
-    buttons.push(button);
-  }
-  return buttons;
+  const items: CollapseProps["items"] = parent.multipleChoice.map((crate) => ({
+    key: crate.contents,
+    label: (
+      <Flex justify={"space-evenly"}>
+        <Image src={crate.image} width={50} height={"auto"} preview={false} />
+        <Typography.Text>{crate.contents}</Typography.Text>
+      </Flex>
+    ),
+    children: (
+      <div>
+        <Image src={crate.image} preview={{ toolbarRender: () => null }} />
+        <Typography.Title level={3}>{crate.contents}</Typography.Title>
+        <Button
+          size={"large"}
+          key={crate.contents}
+          onClick={() => {
+            crateDecoder.addToScannedMultipleChoice(crate.code, crate);
+            crateDecoder.setResult(crate.code, badgeDecoder);
+            setCrateToDisplay(crate);
+            setRenderMultipleChoiceCrateCode(undefined);
+          }}
+        >
+          Select
+        </Button>
+      </div>
+    ),
+  }));
+  return <Collapse items={items} />;
 }
 
 export default MultipeChoiceCrate;
