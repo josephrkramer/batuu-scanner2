c961191b9e30b13f09c2fe737d6b0c55f0b885d0
Joseph Kramer <joseph.ryan.kramer@gmail.com>
2024-09-23 11:42:20 -0400

    Update password-protector.ts (#398)
    * Update password-protector.ts
    
    * Update password-protector.ts

--- src/services/password-protector.ts
+++ src/services/password-protector.ts
@@ -1,10 +1,7 @@
-import { BadgeCode } from "./badge-decoder";
-
 export class PasswordProtector {
   passwords = new Map<string, string>();
 
   constructor() {
-    this.passwords.set(BadgeCode.Jawa, "jawa");
     this.passwords.set("admin", "raithe");
   }
 }
