1e64c72a27a25ffde776f0c0c3d212a0a151c9e4
Joseph Kramer <joseph.ryan.kramer@gmail.com>
2024-09-23 00:37:48 +0000

    update unit tests

--- src/services/badge-decoder.ts
+++ src/services/badge-decoder.ts
@@ -126,7 +126,7 @@ export class BadgeDecoder {
         code: BadgeCode.Relic_Hunter,
         name: "Relic Hunter",
         description:
-          "Find all of the AARC relics hidden in the crates on Batuu.",
+          "Find all of the hidden AARC relics.",
         image: "./badge/relic-hunter.jpeg",
         aztec: "./aztec/Relic_Hunter.png",
       }),
@@ -592,7 +592,7 @@ export class BadgeDecoder {
   }
 
   private relicHunter(crateCode: string, crateDecoder: CrateDecoder) {
-    //Relic Hunter - all Relic "overridden" crates
+    //Relic Hunter - all Relic "overridden" crates and coaster
     if (
       !this.earnedBadges.has(BadgeCode.Relic_Hunter) &&
       this.isLastRelicCrate(crateCode, crateDecoder)
