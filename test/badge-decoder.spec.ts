6e25421e1a33fb2d8fa9e884e322840c62ee7444
Joseph Kramer <joseph.ryan.kramer@gmail.com>
2024-09-26 21:26:47 +0000

    fix unit tests

--- test/badge-decoder.spec.ts
+++ test/badge-decoder.spec.ts
@@ -182,8 +182,8 @@ describe("BadgeDecoder", () => {
         crateDecoder.setResult(crate.code, badgeDecoder);
       });
     });
-    expect(crateDecoder.getTotalNumberOfType(CrateType.Relic)).toBe(15);
-    expect(crateDecoder.getScannedNumberOfType(CrateType.Relic)).toBe(15);
+    expect(crateDecoder.getTotalNumberOfType(CrateType.Relic)).toBe(17);
+    expect(crateDecoder.getScannedNumberOfType(CrateType.Relic)).toBe(17);
     expect(badgeDecoder.earnedBadges.has(BadgeCode.Relic_Hunter)).toBe(true);
   });
 
