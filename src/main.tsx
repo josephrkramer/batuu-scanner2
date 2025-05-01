51c0cbc5371a2977f6a589ba88047b418aeacd2e
Joseph Kramer <joseph.ryan.kramer@gmail.com>
2025-05-01 17:58:54 -0400

    switch to hashrouter (#741)
    * switch to hashrouter
    
    * comment reasoning for hashrouter

--- src/main.tsx
+++ src/main.tsx
@@ -1,15 +1,16 @@
 import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
 import "./index.css";
-import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
+import { HashRouter, Navigate, Route, Routes } from "react-router-dom";
 import DistressCall from "./pages/DistressCall.tsx";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
-  <BrowserRouter>
+  // Using HashRouter to get around GitHub Pages being the primary router for URL paths
+  <HashRouter>
     <Routes>
       <Route path="/" element={<App />} />
       <Route path="distresscall" element={<DistressCall />} />
       <Route path="*" element={<Navigate to="/" />} />
     </Routes>
-  </BrowserRouter>,
+  </HashRouter>,
 );
