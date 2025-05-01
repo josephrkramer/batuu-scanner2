62cd9b17340a4c5f062474127369bd9a48b89faf
Joseph Kramer <joseph.ryan.kramer@gmail.com>
2025-05-01 21:55:20 +0000

    switch to hashrouter

--- src/main.tsx
+++ src/main.tsx
@@ -1,15 +1,15 @@
 import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
 import "./index.css";
-import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
+import { HashRouter, Navigate, Route, Routes } from "react-router-dom";
 import DistressCall from "./pages/DistressCall.tsx";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
-  <BrowserRouter>
+  <HashRouter>
     <Routes>
       <Route path="/" element={<App />} />
       <Route path="distresscall" element={<DistressCall />} />
       <Route path="*" element={<Navigate to="/" />} />
     </Routes>
-  </BrowserRouter>,
+  </HashRouter>,
 );
