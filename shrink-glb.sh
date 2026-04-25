e82841c95d2e969b5d25b447a1023bbadcaafab9
Joseph Kramer <joseph.ryan.kramer@gmail.com>
2026-04-25 10:09:45 -0400

    optimize-glb

---
+++ shrink-glb.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+set -euo pipefail
+
+if [ "$#" -ne 2 ]; then
+  echo "Usage: $0 <input.glb> <output.glb>"
+  exit 1
+fi
+
+npm install --global @gltf-transform/cli
+
+gltf-transform optimize "$1" "$2" \
+  --simplify 0.5 \
+  --texture-compress webp \
+  --texture-size 1024 \
+  --compress false
